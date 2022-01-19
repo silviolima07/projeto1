b3ff4d3135b245b215388120edcc0c9b5d3ac2e1
silviolima07 <silviolima07@gmail.com>
2022-01-19 16:02:38 -0300

    new app

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import docx2txt
 from PIL import Image 
 from PyPDF2 import PdfFileReader
 import pdfplumber
@@ -41,7 +40,7 @@ def load_image(image_file):
 def main():
 	st.title("File Upload Tutorial")
 
-	menu = ["Home","Dataset","DocumentFiles","About"]
+	menu = ["Home","Dataset","About"]
 	choice = st.sidebar.selectbox("Menu",menu)
 
 	if choice == "Home":
@@ -70,37 +69,6 @@ def main():
 				df = pd.read_csv(data_file)
 				st.dataframe(df)
 
-	elif choice == "DocumentFiles":
-		st.subheader("DocumentFiles")
-		docx_file = st.file_uploader("Upload File",type=['txt','docx','pdf'])
-		if st.button("Process"):
-			if docx_file is not None:
-				file_details = {"Filename":docx_file.name,"FileType":docx_file.type,"FileSize":docx_file.size}
-				st.write(file_details)
-				# Check File Type
-				if docx_file.type == "text/plain":
-					# raw_text = docx_file.read() # read as bytes
-					# st.write(raw_text)
-					# st.text(raw_text) # fails
-					st.text(str(docx_file.read(),"utf-8")) # empty
-					raw_text = str(docx_file.read(),"utf-8") # works with st.text and st.write,used for futher processing
-					# st.text(raw_text) # Works
-					st.write(raw_text) # works
-				elif docx_file.type == "application/pdf":
-					# raw_text = read_pdf(docx_file)
-					# st.write(raw_text)
-					try:
-						with pdfplumber.open(docx_file) as pdf:
-						    page = pdf.pages[0]
-						    st.write(page.extract_text())
-					except:
-						st.write("None")
-					    
-					
-				elif docx_file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
-				# Use the right file processor ( Docx,Docx2Text,etc)
-					raw_text = docx2txt.process(docx_file) # Parse in the uploadFile Class directory
-					st.write(raw_text)
 
 	else:
 		st.subheader("About")
