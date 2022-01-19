a81b20a0aa6ddfb5a5d236a1a03c6a6279a71218
silviolima07 <silviolima07@gmail.com>
2022-01-19 16:00:42 -0300

    new app

--- app.py
+++ app.py
@@ -1,31 +1,4 @@
 import streamlit as st
-from io import StringIO
-import pandas as pd
-
-"""
-
-st.title("Dataset csv")
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-     # To read file as bytes:
-     bytes_data = uploaded_file.getvalue()
-     #st.write(bytes_data)
-
-     # To convert to a string based IO:
-     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-     #st.write(stringio)
-
-     # To read file as string:
-     string_data = stringio.read()
-     #st.write(string_data)
-
-     # Can be used wherever a "file-like" object is accepted:
-     dataframe = pd.read_csv(uploaded_file)
-     st.table(dataframe)
-     
-"""
-
-# File Processing Pkgs
 import pandas as pd
 import docx2txt
 from PIL import Image 
